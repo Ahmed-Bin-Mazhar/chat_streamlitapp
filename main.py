df4b961d8fde691f6c8072a20fe6a6a69ea16fce
Ahmed Bin Mazhar <ahmedbinmazhar@gmail.com>
2025-08-16 06:37:08 +0000

    pdf file
    pdf file extractor

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-from datetime import datetime
-import pandas as pd
 import streamlit as st
+import PyPDF2
 
-data_df = pd.DataFrame(
-    {
-        "appointment": [
-            datetime(2024, 2, 5, 12, 30),
-            datetime(2023, 11, 10, 18, 0),
-            datetime(2024, 3, 11, 20, 10),
-            datetime(2023, 9, 12, 3, 0),
-        ]
-    }
-)
+def read_pdf(file):
+    pdf_reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
 
-st.data_editor(
-    data_df,
-    column_config={
-        "appointment": st.column_config.DatetimeColumn(
-            "Appointment",
-            min_value=datetime(2023, 6, 1),
-            max_value=datetime(2025, 1, 1),
-            format="D MMM YYYY, h:mm a",
-            step=60,
-        ),
-    },
-    hide_index=True,
-)
+st.title("PDF Reader App")
+
+st.write("Upload a PDF file to read its content.")
+
+try:
+   uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+   
+   if uploaded_file:
+      pdf_text = read_pdf(uploaded_file)
+      st.write(pdf_text)
+    
+
+
+
+
+except Exception as ex:
+    print("[+] Exception :{str(ex)}")
